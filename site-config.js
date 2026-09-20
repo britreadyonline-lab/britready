6883fd959c007fe07513e07f6502777fd08f537f
BritReady Online <britreadyonline@gmail.com>
2026-09-20 16:45:23 +0100

    Update site for 5:15 evening spotting

--- site-config.js
+++ site-config.js
@@ -8,15 +8,15 @@ window.BRITREADY_SITE_CONFIG = {
     state: "upcoming",
     airport: "London Heathrow Airport",
     shortAirport: "London Heathrow",
-    startsAt: "2026-09-20T12:30:00+01:00",
-    endsAt: "2026-09-20T14:50:00+01:00",
+    startsAt: "2026-09-20T17:15:00+01:00",
+    endsAt: "",
     youtubeUrl: "https://youtube.com/live/YvPX6QQWfUY?feature=share",
     youtubeEmbedUrl: "https://www.youtube-nocookie.com/embed/YvPX6QQWfUY?rel=0&autoplay=1"
   },
   nextSpotting: {
     airport: "London Heathrow Airport",
-    startsAt: "2026-09-20T12:30:00+01:00",
-    endsAt: "2026-09-20T14:50:00+01:00",
+    startsAt: "2026-09-20T17:15:00+01:00",
+    endsAt: "",
     status: "Scheduled",
     youtubeUrl: "https://youtube.com/live/YvPX6QQWfUY?feature=share"
   },
